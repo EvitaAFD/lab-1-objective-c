db89cf7bf5619c5bde198e9413ffa413544e6ea1
EVE DENISON <evedenison@gmail.com>
2017-04-22 21:06:56 -0700

    added kvo observer counter

---
+++ lab-1-objective-c/lab-1-objective-c/Count.h
@@ -0,0 +1,17 @@
+//
+//  Count.h
+//  lab-1-objective-c
+//
+//  Created by Eve Denison on 4/22/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Count : NSObject
+
+@property (nonatomic) NSInteger count;
+
+-(void)countIncrements;
+
+@end
--- lab-1-objective-c/lab-1-objective-c/ViewController.m
+++ lab-1-objective-c/lab-1-objective-c/ViewController.m
@@ -10,8 +10,13 @@
 #import "NSString+String.h"
 #import "EmployeeDatabase.h"
 #import "Employee.h"
+#import "Count.h"
+
+static void *kvoContext = &kvoContext;
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property(strong, nonatomic)Count *counter;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 
@@ -27,6 +32,34 @@
     
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(reloadTableData) name:@"reloadData" object:nil];
     NSLog(@"%@", [[EmployeeDatabase shared] allEmployees]);
+    
+     self.counter = [[Count alloc] init];
+    
+    [self setValue:@0 forKeyPath:@"counter.count"];
+    NSLog(@"%i", (int)self.counter.count);
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self addObserver:self forKeyPath:@"self.counter.count" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:kvoContext];
+    
+    [self.counter countIncrements];
+    
+    
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    
+    if (context == kvoContext) {
+        
+        NSLog(@"Value for Count Changed: %i", (int)self.counter.count);
+        
+        
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+    
 }
 
 -(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
